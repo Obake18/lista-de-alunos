106ca9ad69b1403eb04775364927dd5fcbc08382
Obake18 <miltondesenhista@gmail.com>
2023-08-29 15:36:24 -0300

    Sei lá

---
+++ classes.js
@@ -0,0 +1,11 @@
+class Aluno{
+    constructor(foto, nome, idade, cidade){
+        this.foto = foto;
+        this.nome = nome;
+        this.idade = idade;
+        this.cidade = cidade;
+    }
+
+}
+
+
---
+++ programa.js
@@ -0,0 +1,12 @@
+let aluno1 = new Aluno ('foto1.jpg', "Alice", "19", "Amsterdam")
+let aluno2 = new Aluno ('foto2.jpg', "Benjamin", "21", "Barcelona")
+let aluno3 = new Aluno ('foto3.jpg', "Clara", "18", "Chicago")
+let aluno4 = new Aluno ('foto4.jpg', "Daniel", "24","Dublin")
+let aluno5 = new Aluno ('foto5.jpg', "Emily", "20", "Edinburgh")
+let aluno6 = new Aluno ('foto6.jpg', "Felipe", "25","Florença")
+let aluno7 = new Aluno ('foto7.jpg', "Grace","22", "Honolulu")
+let aluno8 = new Aluno ('foto8.jpg', "Henry", "17","Instanbul")
+let aluno9 = new Aluno ('foto9.jpg', "Isabella", "27", "Kyoto")
+let aluno10 = new Aluno ('foto10', "Jack", "28", "Lisboa")
+
+let 
