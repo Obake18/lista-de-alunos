955e18612696030a96668283f03b26709d214f8f
Obake18 <miltondesenhista@gmail.com>
2023-09-05 14:37:53 -0300

    Subindo fotos

--- programa.js
+++ programa.js
@@ -1,13 +1,13 @@
-let aluno1 = new Aluno('foto1.jpg', "Sujiro Kimimame" , "19", "Yokohama");
-let aluno2 = new Aluno('foto2.jpg', "Kabe Sadul Meup'Al", "21", "Cairo");
-let aluno3 = new Aluno('foto3.jpg', "Jaq-omi Sua-MAin", "18", "Arábia Saudita");
-let aluno4 = new Aluno('foto4.jpg', "Telin Habei", "24", "Qatar");
-let aluno5 = new Aluno('foto5.jpg', "Seoku Miyadora", "20", "Kyoto");
-let aluno6 = new Aluno('foto6.jpg', "Tomas Turbano", "25", "Maceió");
-let aluno7 = new Aluno('foto7.jpg', "Paula Tejano", "22", "Maceió");
-let aluno8 = new Aluno('foto8.jpg', "H. Romeu Pinto", "17", "Rio Largo");
-let aluno9 = new Aluno('foto9.jpg', "Ahom Baku", "27", "Kyoto");
-let aluno10 = new Aluno('foto10.jp', "Jalin Habei", "28", "Nova Dheli");
+let aluno1 = new Aluno('foto1.jpg', "Sujiro", " Kimimame" , "19", "Yokohama");
+let aluno2 = new Aluno('foto2.jpg', "Kabe Sadul", " Meup'Al", "21", "Cairo");
+let aluno3 = new Aluno('foto3.jpg', "Jaq-omi", " Sua-MAin", "18", "Arábia Saudita");
+let aluno4 = new Aluno('foto4.jpg', "Telin", " Habei", "24", "Qatar");
+let aluno5 = new Aluno('foto5.jpg', "Seoku", " Miyadora", "20", "Kyoto");
+let aluno6 = new Aluno('foto6.jpg', "Tomas", " Turbano", "25", "Maceió");
+let aluno7 = new Aluno('foto7.jpg', "Peneirinha", "Da Silva", "22", "Maceió");
+let aluno8 = new Aluno('foto8.jpg', "H.", " Romeu Pinto", "17", "Rio Largo");
+let aluno9 = new Aluno('foto9.jpg', "Ahom", " Baku", "27", "Kyoto");
+let aluno10 = new Aluno('foto10.jpg', "Jalin", " Habei", "28", "Nova Dheli");
 
 let listaDeAluno = [];
 
@@ -26,7 +26,9 @@ for (let i = 0; i < listaDeAluno.length; i++) {
     document.write(`
     <div class="card">
         <img class="foto" src="img/${listaDeAluno[i].foto}" />
-        <div class="nome"><h1>${listaDeAluno[i].nome}</h1></div>
+        <div class="nome"><h1>${listaDeAluno[i].primeiroNome}${listaDeAluno[i].segundoNome}</h1></div>
+        
+
         <div><h3>${listaDeAluno[i].idade}</h3></div>
         <div><h3>${listaDeAluno[i].cidade}</h3></div>
     </div>
